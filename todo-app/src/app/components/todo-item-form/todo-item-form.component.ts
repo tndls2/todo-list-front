6ad8ebf3f3d895736b2e99cc6e481089d6db4a63
suin park <tndls3026@ajou.ac.kr>
2024-10-19 21:21:44 +0900

    [init] 앵귤러 기본 세팅

---
+++ todo-app/src/app/components/todo-item-form/todo-item-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item-form.component.html',
+  styleUrl: './todo-item-form.component.css'
+})
+export class TodoItemFormComponent {
+
+}
